af07b085924481948f524faee5176b78c09944f0
ub97onag <maike.boehm@fau.de>
2022-12-21 11:59:02 +0100

    Sixth commit

--- asm1.py
+++ asm1.py
@@ -1,54 +1,51 @@
 import numpy as np
-import math
 from scipy.integrate import solve_ivp, odeint
 from numba import jit, njit
+import nbkode
 
 
 # differential equations in function because @jit does not compile class methods in nopython mode
 @jit(nopython=True)
 def derivatives_function(t, y, y_in, parasm, kla, volume, sosat):
-    S_I_in, S_S_in, X_I_in, X_S_in, X_BH_in, X_BA_in, X_P_in, S_O_in, S_NO_in, S_NH_in, S_ND_in, X_ND_in, S_ALK_in, TSS_in, Q_in, T_in, S_D1_in, S_D2_in, S_D3_in, X_D4_in, X_D5_in = y_in
+    dy = np.zeros(21)
+
     mu_H, K_S, K_OH, K_NO, b_H, mu_A, K_NH, K_OA, b_A, ny_g, k_a, k_h, K_X, ny_h, Y_H, Y_A, f_P, i_XB, i_XP, X_I2TSS, X_S2TSS, X_BH2TSS, X_BA2TSS, X_P2TSS = parasm
     # add decay parameter here
 
     # temperature compensation:
-    mu_H = mu_H * math.exp((math.log(mu_H / 3.0) / 5.0) * (T_in - 15.0))  # temperature at the influent of the reactor
-    b_H = b_H * math.exp((math.log(b_H / 0.2) / 5.0) * (T_in - 15.0))
-    mu_A = mu_A * math.exp((math.log(mu_A / 0.3) / 5.0) * (T_in - 15.0))
-    b_A = b_A * math.exp((math.log(b_A / 0.03) / 5.0) * (T_in - 15.0))
-    k_h = k_h * math.exp((math.log(k_h / 2.5) / 5.0) * (T_in - 15.0))
-    k_a = k_a * math.exp((math.log(k_a / 0.04) / 5.0) * (T_in - 15.0))
-    SO_sat_temp = 0.9997743214 * 8.0 / 10.5 * (56.12 * 6791.5 * math.exp(
-        -66.7354 + 87.4755 / ((T_in + 273.15) / 100.0) + 24.4526 * math.log(
-            (T_in + 273.15) / 100.0)))  # van't Hoff equation
-    KLa_temp = kla * pow(1.024, (T_in - 15.0))
+    mu_H = mu_H * np.exp((np.log(mu_H / 3.0) / 5.0) * (y_in[15] - 15.0))  # temperature at the influent of the reactor
+    b_H = b_H * np.exp((np.log(b_H / 0.2) / 5.0) * (y_in[15] - 15.0))
+    mu_A = mu_A * np.exp((np.log(mu_A / 0.3) / 5.0) * (y_in[15] - 15.0))
+    b_A = b_A * np.exp((np.log(b_A / 0.03) / 5.0) * (y_in[15] - 15.0))
+    k_h = k_h * np.exp((np.log(k_h / 2.5) / 5.0) * (y_in[15] - 15.0))
+    k_a = k_a * np.exp((np.log(k_a / 0.04) / 5.0) * (y_in[15] - 15.0))
+    SO_sat_temp = 0.9997743214 * 8.0 / 10.5 * (56.12 * 6791.5 * np.exp(
+        -66.7354 + 87.4755 / ((y_in[15] + 273.15) / 100.0) + 24.4526 * np.log(
+            (y_in[15] + 273.15) / 100.0)))  # van't Hoff equation
+    KLa_temp = kla * np.power(1.024, (y_in[15] - 15.0))
 
     # TEMPMODEL hier einbauen
 
     # analog zu Matlab:
-    y_temp = y
-    y_temp[y_temp < 0.0] = 0.0
-
-    S_I_temp, S_S_temp, X_I_temp, X_S_temp, X_BH_temp, X_BA_temp, X_P_temp, S_O_temp, S_NO_temp, S_NH_temp, S_ND_temp, X_ND_temp, S_ALK_temp, TSS_temp, Q_temp, T_temp, S_D1_temp, S_D2_temp, S_D3_temp, X_D4_temp, X_D5_temp = y_temp
+    xtemp = y
+    xtemp[xtemp < 0.0] = 0.0
 
     # bestimmte O2-Konzentration wird gehalten, wenn KLa negativ ist:
     if kla < 0.0:
         y[7] = abs(kla)
 
-    S_I, S_S, X_I, X_S, X_BH, X_BA, X_P, S_O, S_NO, S_NH, S_ND, X_ND, S_ALK, TSS, Q, T, S_D1, S_D2, S_D3, X_D4, X_D5 = y
-
     # process rates:
-    proc1 = mu_H * (S_S_temp / (K_S + S_S_temp)) * (S_O_temp / (K_OH + S_O_temp)) * X_BH_temp
-    proc2 = mu_H * (S_S_temp / (K_S + S_S_temp)) * (K_OH / (K_OH + S_O_temp)) * (
-                S_NO_temp / (K_NO + S_NO_temp)) * ny_g * X_BH_temp
-    proc3 = mu_A * (S_NH_temp / (K_NH + S_NH_temp)) * (S_O_temp / (K_OA + S_O_temp)) * X_BA_temp
-    proc4 = b_H * X_BH_temp
-    proc5 = b_A * X_BA_temp
-    proc6 = k_a * S_ND_temp * X_BH_temp
-    proc7 = k_h * ((X_S_temp / X_BH_temp) / (K_X + (X_S_temp / X_BH_temp))) * (
-                (S_O_temp / (K_OH + S_O_temp)) + ny_h * (K_OH / (K_OH + S_O_temp)) * (
-                    S_NO_temp / (K_NO + S_NO_temp))) * X_BH_temp
-    proc8 = proc7 * (X_ND_temp / X_S_temp)
+    proc1 = mu_H * (xtemp[1] / (K_S + xtemp[1])) * (xtemp[7] / (K_OH + xtemp[7])) * xtemp[4]
+    proc2 = mu_H * (xtemp[1] / (K_S + xtemp[1])) * (K_OH / (K_OH + xtemp[7])) * (
+                xtemp[8] / (K_NO + xtemp[8])) * ny_g * xtemp[4]
+    proc3 = mu_A * (xtemp[9] / (K_NH + xtemp[9])) * (xtemp[7] / (K_OA + xtemp[7])) * xtemp[5]
+    proc4 = b_H * xtemp[4]
+    proc5 = b_A * xtemp[5]
+    proc6 = k_a * xtemp[10] * xtemp[4]
+    proc7 = k_h * ((xtemp[3] / xtemp[4]) / (K_X + (xtemp[3] / xtemp[4]))) * (
+            (xtemp[7] / (K_OH + xtemp[7])) + ny_h * (K_OH / (K_OH + xtemp[7])) * (
+                    xtemp[8] / (K_NO + xtemp[8]))) * xtemp[4]
+    proc8 = proc7 * (xtemp[11] / xtemp[3])
 
     # conversion rates:
     reac1 = 0.0
@@ -73,60 +70,55 @@ def derivatives_function(t, y, y_in, parasm, kla, volume, sosat):
     reac20 = 0.0
 
     # differential equations:
-    dS_I = 1.0 / volume * (Q_in * (S_I_in - S_I)) + reac1
-    dS_S = 1.0 / volume * (Q_in * (S_S_in - S_S)) + reac2
-    dX_I = 1.0 / volume * (Q_in * (X_I_in - X_I)) + reac3
-    dX_S = 1.0 / volume * (Q_in * (X_S_in - X_S)) + reac4
-    dX_BH = 1.0 / volume * (Q_in * (X_BH_in - X_BH)) + reac5
-    dX_BA = 1.0 / volume * (Q_in * (X_BA_in - X_BA)) + reac6
-    dX_P = 1.0 / volume * (Q_in * (X_P_in - X_P)) + reac7
+    dy[0] = 1.0 / volume * (y_in[14] * (y_in[0] - y[0])) + reac1
+    dy[1] = 1.0 / volume * (y_in[14] * (y_in[1] - y[1])) + reac2
+    dy[2] = 1.0 / volume * (y_in[14] * (y_in[2] - y[2])) + reac3
+    dy[3] = 1.0 / volume * (y_in[14] * (y_in[3] - y[3])) + reac4
+    dy[4] = 1.0 / volume * (y_in[14] * (y_in[4] - y[4])) + reac5
+    dy[5] = 1.0 / volume * (y_in[14] * (y_in[5] - y[5])) + reac6
+    dy[6] = 1.0 / volume * (y_in[14] * (y_in[6] - y[6])) + reac7
     if kla < 0.0:
-        dS_O = 0.0
+        dy[7] = 0.0
     else:
-        dS_O = 1.0 / volume * (Q_in * (S_O_in - S_O)) + reac8 + KLa_temp * (SO_sat_temp - S_O)
-    dS_NO = 1.0 / volume * (Q_in * (S_NO_in - S_NO)) + reac9
-    dS_NH = 1.0 / volume * (Q_in * (S_NH_in - S_NH)) + reac10
-    dS_ND = 1.0 / volume * (Q_in * (S_ND_in - S_ND)) + reac11
-    dX_ND = 1.0 / volume * (Q_in * (X_ND_in - X_ND)) + reac12
-    dS_ALK = 1.0 / volume * (Q_in * (S_ALK_in - S_ALK)) + reac13
-    dTSS = 0.0
-    dQ = 0.0
-    dT = 0.0  # TEMPMODEL hier einbauen
+        dy[7] = 1.0 / volume * (y_in[14] * (y_in[7] - y[7])) + reac8 + KLa_temp * (SO_sat_temp - y[7])
+    dy[8] = 1.0 / volume * (y_in[14] * (y_in[8] - y[8])) + reac9
+    dy[9] = 1.0 / volume * (y_in[14] * (y_in[9] - y[9])) + reac10
+    dy[10] = 1.0 / volume * (y_in[14] * (y_in[10] - y[10])) + reac11
+    dy[11] = 1.0 / volume * (y_in[14] * (y_in[11] - y[11])) + reac12
+    dy[12] = 1.0 / volume * (y_in[14] * (y_in[12] - y[12])) + reac13
+    dy[13] = 0.0
+    dy[14] = 0.0
+    dy[15] = 0.0  # TEMPMODEL hier einbauen
     # ACTIVATE hier einbauen, damit er nicht integrieren muss??
-    dS_D1 = 1.0 / volume * (Q_in * (S_D1_in - S_D1)) + reac16
-    dS_D2 = 1.0 / volume * (Q_in * (S_D2_in - S_D2)) + reac17
-    dS_D3 = 1.0 / volume * (Q_in * (S_D3_in - S_D3)) + reac18
-    dX_D4 = 1.0 / volume * (Q_in * (X_D4_in - X_D4)) + reac19
-    dX_D5 = 1.0 / volume * (Q_in * (X_D5_in - X_D5)) + reac20
+    dy[16] = 1.0 / volume * (y_in[14] * (y_in[16] - y[16])) + reac16
+    dy[17] = 1.0 / volume * (y_in[14] * (y_in[17] - y[17])) + reac17
+    dy[18] = 1.0 / volume * (y_in[14] * (y_in[18] - y[18])) + reac18
+    dy[19] = 1.0 / volume * (y_in[14] * (y_in[19] - y[19])) + reac19
+    dy[20] = 1.0 / volume * (y_in[14] * (y_in[20] - y[20])) + reac20
 
-    f = [dS_I, dS_S, dX_I, dX_S, dX_BH, dX_BA, dX_P, dS_O, dS_NO, dS_NH, dS_ND, dX_ND,
-         dS_ALK, dTSS, dQ, dT, dS_D1, dS_D2, dS_D3, dX_D4, dX_D5]
-
-    return f
+    return dy
 
 
 class ASM1reactor:
 
-    def __init__(self, volume, kla, sosat, y0):
-        self.volume = volume    # volume of the reactor compartment
-        self.kla = kla       # oxygen transfer coefficient in aerated reactors
-        self.sosat = sosat      # saturation concentration for oxygen
-        self.y0 = y0        # initial states for integration (damit man es anpassen kann)
+    def __init__(self, volume, kla, sosat, y0, parasm):
+        self.volume = volume        # volume of the reactor compartment
+        self.kla = kla              # oxygen transfer coefficient in aerated reactors
+        self.sosat = sosat          # saturation concentration for oxygen
+        self.y0 = y0                # initial states for integration (damit man es anpassen kann)
+        self.parasm = parasm        # Parameters of ASM1-Model
 
-    def derivatives(self, t, y, y_in, parasm):
-        return derivatives_function(t, y, y_in, parasm, self.kla, self.volume, self.sosat)
-
-    def output(self, timestep, step, y_in, parasm):
+    def output(self, timestep, step, y_in):
         t_span = [step-timestep, step]
         t_eval = np.array([step-timestep, step])
 
-        sol = solve_ivp(self.derivatives, t_span, self.y0, args=(y_in, parasm,), rtol=1e-5, atol=1e-8)
-        y_out = np.array([x[1] for x in sol.y])
+        # sol = solve_ivp(derivatives_function, t_span, self.y0, args=(y_in, self.parasm,self.kla, self.volume, self.sosat,), rtol=1e-5, atol=1e-8)
+        # y_out = np.array([x[1] for x in sol.y])
 
-        # ode = odeint(self.derivatives, self.y0, t_eval, tfirst=True, args=(y_in, parasm,), rtol=1e-5, atol=1e-8)
-        # y_out = ode[1]
+        ode = odeint(derivatives_function, self.y0, t_eval, tfirst=True, args=(y_in, self.parasm, self.kla, self.volume, self.sosat,), rtol=1e-5, atol=1e-8)
+        y_out = ode[1]
 
-        y_out[13] = parasm[19] * y_out[2] + parasm[20] * y_out[3] + parasm[21] * y_out[4] + parasm[22] * y_out[5] + parasm[23] * y_out[6]  # TSS
+        y_out[13] = self.parasm[19] * y_out[2] + self.parasm[20] * y_out[3] + self.parasm[21] * y_out[4] + self.parasm[22] * y_out[5] + self.parasm[23] * y_out[6]  # TSS
         y_out[14] = y_in[14]  # Flow
         y_out[15] = y_in[15]  # Temperature, TEMPMODEL fehlt hier noch
         self.y0 = y_out
--- asm1run.py
+++ asm1run.py
@@ -7,92 +7,110 @@ import asm1
 import settler
 import xlsxwriter
 
-PAR = asm1init.PAR  # in Matlab wird nochmal aufgeteilt in die Reaktoren (aber ist immer gleich)
-
 # Anfangswerte aus Matlab für Dynamische Simulation:
 
-df_init = pd.read_excel('results_ss.xlsx', header=None)
-XINIT1_list = df_init.loc[0, 0:20].values.tolist()
-XINIT1 = np.array(XINIT1_list)
-XINIT2_list = df_init.loc[1, 0:20].values.tolist()
-XINIT2 = np.array(XINIT2_list)
-XINIT3_list = df_init.loc[2, 0:20].values.tolist()
-XINIT3 = np.array(XINIT3_list)
-XINIT4_list = df_init.loc[3, 0:20].values.tolist()
-XINIT4 = np.array(XINIT4_list)
-XINIT5_list = df_init.loc[4, 0:20].values.tolist()
-XINIT5 = np.array(XINIT5_list)
-SETTLERINIT_list = df_init.loc[6].values.tolist()
-SETTLERINIT = np.array(SETTLERINIT_list)
+df_init = pd.read_excel('results_ss_ode.xlsx', header=None)
+YINIT1 = df_init.loc[0, 0:20].to_numpy()
+YINIT2 = df_init.loc[1, 0:20].to_numpy()
+YINIT3 = df_init.loc[2, 0:20].to_numpy()
+YINIT4 = df_init.loc[3, 0:20].to_numpy()
+YINIT5 = df_init.loc[4, 0:20].to_numpy()
+SETTLERINIT = df_init.loc[6].to_numpy()
+
 
 # definition of the reactors:
 
-reactor1 = asm1.ASM1reactor(asm1init.VOL1, asm1init.KLa1, asm1init.SOSAT1, XINIT1)
-reactor2 = asm1.ASM1reactor(asm1init.VOL2, asm1init.KLa2, asm1init.SOSAT2, XINIT2)
-reactor3 = asm1.ASM1reactor(asm1init.VOL3, asm1init.KLa3, asm1init.SOSAT3, XINIT3)
-reactor4 = asm1.ASM1reactor(asm1init.VOL4, asm1init.KLa4, asm1init.SOSAT4, XINIT4)
-reactor5 = asm1.ASM1reactor(asm1init.VOL5, asm1init.KLa5, asm1init.SOSAT5, XINIT5)
-settler = settler.Settler(settlerinit.DIM, settlerinit.LAYER, asm1init.Qr, asm1init.Qw, SETTLERINIT)
+reactor1 = asm1.ASM1reactor(asm1init.VOL1, asm1init.KLa1, asm1init.SOSAT1, YINIT1, asm1init.PAR1)
+reactor2 = asm1.ASM1reactor(asm1init.VOL2, asm1init.KLa2, asm1init.SOSAT2, YINIT2, asm1init.PAR2)
+reactor3 = asm1.ASM1reactor(asm1init.VOL3, asm1init.KLa3, asm1init.SOSAT3, YINIT3, asm1init.PAR3)
+reactor4 = asm1.ASM1reactor(asm1init.VOL4, asm1init.KLa4, asm1init.SOSAT4, YINIT4, asm1init.PAR4)
+reactor5 = asm1.ASM1reactor(asm1init.VOL5, asm1init.KLa5, asm1init.SOSAT5, YINIT5, asm1init.PAR5)
+settler = settler.Settler(settlerinit.DIM, settlerinit.LAYER, asm1init.Qr, asm1init.Qw, SETTLERINIT, settlerinit.SETTLERPAR)
 
 # Dynamic Influent (Dryinfluent):
 df = pd.read_excel('dryinfluent.xlsx', 'Tabelle1', header=None)
-# df2 = pd.read_excel('sensornoise.xlsx', 'Tabelle1', header=None) --> mit row2
 
 timestep = 1/(60*24)
-simtime = np.arange(timestep, 14, timestep)
-y_out5 = XINIT5
-y_outs_list = df_init.loc[5, 0:20].values.tolist()
-y_outs = np.array(y_outs_list)
+endtime = 14
+simtime = np.arange(timestep, endtime, timestep)
+y_out5 = YINIT5
+ys_out = df_init.loc[5, 0:20].to_numpy()
 Qintr = 55338
-number = 1
+numberstep = 1
 
 start = time.perf_counter()
 row = 0
-y_in_list = df.loc[row, 1:21].values.tolist()
-y_in = np.array(y_in_list)
+y_in = df.loc[row, 1:21].to_numpy()
 
-# i = 0
-# wb = xlsxwriter.Workbook('results_14.xlsx')
-# ws = wb.add_worksheet('yin1')
-# ws1 = wb.add_worksheet('yout1')
-# ws2 = wb.add_worksheet('yout2')
-# ws3 = wb.add_worksheet('yout3')
-# ws4 = wb.add_worksheet('yout4')
-# ws5 = wb.add_worksheet('yout5')
+reactin = np.zeros((int(endtime/timestep), 21))
+react1 = np.zeros((int(endtime/timestep), 21))
+react2 = np.zeros((int(endtime/timestep), 21))
+react3 = np.zeros((int(endtime/timestep), 21))
+react4 = np.zeros((int(endtime/timestep), 21))
+react5 = np.zeros((int(endtime/timestep), 21))
+settlerout = np.zeros((int(endtime/timestep), 21))
+settlerall = np.zeros((int(endtime/timestep), 203))
+
+number = 0
+
 for step in simtime:
-    if number % 1440 == 0.0:
-        Zeit = time.ctime()
-        print('Aktueller Schritt bei', Zeit, ':', step)
-    if number % 15 == 0.0:
+    if numberstep % 15 == 0.0:
         row = row + 1
-        y_in_list = df.loc[row, 1:21].values.tolist()
-        y_in = np.array(y_in_list)
-    y_in_r = (y_in * y_in[14] + y_outs * y_outs[14]) / (y_in[14] + y_outs[14])
-    y_in_r[14] = y_in[14] + y_outs[14]
+        y_in = df.loc[row, 1:21].to_numpy()
+    y_in_r = (y_in * y_in[14] + ys_out * ys_out[14]) / (y_in[14] + ys_out[14])
+    y_in_r[14] = y_in[14] + ys_out[14]
     y_in1 = (y_in_r * y_in_r[14] + y_out5 * Qintr) / (y_in_r[14] + Qintr)
     y_in1[14] = y_in_r[14] + Qintr
-    y_out1 = reactor1.output(timestep, step, y_in1, PAR)
-    y_out2 = reactor2.output(timestep, step, y_out1, PAR)
-    y_out3 = reactor3.output(timestep, step, y_out2, PAR)
-    y_out4 = reactor4.output(timestep, step, y_out3, PAR)
-    y_out5 = reactor5.output(timestep, step, y_out4, PAR)
+
+    reactin[[number]] = y_in1
+
+    y_out1 = reactor1.output(timestep, step, y_in1)
+    y_out2 = reactor2.output(timestep, step, y_out1)
+    y_out3 = reactor3.output(timestep, step, y_out2)
+    y_out4 = reactor4.output(timestep, step, y_out3)
+    y_out5 = reactor5.output(timestep, step, y_out4)
+
+    react1[[number]] = y_out1
+    react2[[number]] = y_out2
+    react3[[number]] = y_out3
+    react4[[number]] = y_out4
+    react5[[number]] = y_out5
+
+    ys_in = y_out5
+    ys_in[14] = ys_in[14] - Qintr
+    ys_out, ys_out_all = settler.outputs(timestep, step, ys_in)
+    settlerout[[number]] = ys_out
+    settlerall[[number]] = ys_out_all
+
+    numberstep = numberstep + 1
     number = number + 1
-    # row = row + 1
-#     ws.write_row(i, 0, y_in1)
-#     ws1.write_row(i, 0, y_out1)
-#     ws2.write_row(i, 0, y_out2)
-#     ws3.write_row(i, 0, y_out3)
-#     ws4.write_row(i, 0, y_out4)
-#     ws5.write_row(i, 0, y_out5)
 
-    y_ins = y_out5
-    y_ins[14] = y_ins[14] - Qintr
-    y_outs = settler.output(timestep, step, y_ins, settlerinit.SETTLERPAR)
-    # ws6.write_row(i, 0, y_outs)
-    # i = i + 1
-
-# wb.close()
 stop = time.perf_counter()
 
 print('Simulationszeit: ', stop - start)
-print('Output bei t = 14 d: ', y_outs)
+print('Output bei t = 14 d: ', ys_out)
+
+start_writer = time.perf_counter()
+
+dfin = pd.DataFrame(reactin)
+df1 = pd.DataFrame(react1)
+df2 = pd.DataFrame(react2)
+df3 = pd.DataFrame(react3)
+df4 = pd.DataFrame(react4)
+df5 = pd.DataFrame(react5)
+dfs = pd.DataFrame(settlerout)
+dfs_all = pd.DataFrame(settlerall)
+with pd.ExcelWriter('results_all_ode.xlsx') as writer:
+    dfin.to_excel(writer, sheet_name='yin', header=False, index=False)
+    df1.to_excel(writer, sheet_name='yout1', header=False, index=False)
+    df2.to_excel(writer, sheet_name='yout2', header=False, index=False)
+    df3.to_excel(writer, sheet_name='yout3', header=False, index=False)
+    df4.to_excel(writer, sheet_name='yout4', header=False, index=False)
+    df5.to_excel(writer, sheet_name='yout5', header=False, index=False)
+    dfs.to_excel(writer, sheet_name='ysout', header=False, index=False)
+    dfs_all.to_excel(writer, sheet_name='ysout_all', header=False, index=False)
+
+stop_writer = time.perf_counter()
+
+print('Zeit für Excel: ', stop_writer - start_writer)
+
