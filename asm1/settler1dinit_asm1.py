7d6c75accd836b6275aeb9cecc9598d1aed02299
ub97onag <maike.boehm@fau.de>
2023-02-22 12:49:11 +0100

    Fourth commit

--- asm1/settler1dinit_asm1.py
+++ asm1/settler1dinit_asm1.py
@@ -1,6 +1,6 @@
 """Initialisation file for all states and parameters related to the secondary clarifier.
 
-All state values are based on Ales et al. (2018), from BSM2 openloop results using the constant input file.
+All parameters and specifications are based on BSM1 model.
 
 This file will be executed when running the asm1run or asm1runss file.
 """
@@ -8,228 +8,9 @@ This file will be executed when running the asm1run or asm1runss file.
 
 import numpy as np
 
-SI_1 = 28.0643
-SI_2 = 28.0643
-SI_3 = 28.0643
-SI_4 = 28.0643
-SI_5 = 28.0643
-SI_6 = 28.0643
-SI_7 = 28.0643
-SI_8 = 28.0643
-SI_9 = 28.0643
-SI_10 = 28.0643
-
-SS_1 = 0.6734
-SS_2 = 0.6734
-SS_3 = 0.6734
-SS_4 = 0.6734
-SS_5 = 0.6734
-SS_6 = 0.6734
-SS_7 = 0.6734
-SS_8 = 0.6734
-SS_9 = 0.6734
-SS_10 = 0.6734
-
-XI_1 = 5
-XI_2 = 10
-XI_3 = 20
-XI_4 = 20
-XI_5 = 200
-XI_6 = 200
-XI_7 = 200
-XI_8 = 200
-XI_9 = 200
-XI_10 = 3000
-
-XS_1 = 0.5
-XS_2 = 1
-XS_3 = 1
-XS_4 = 1
-XS_5 = 4
-XS_6 = 4
-XS_7 = 4
-XS_8 = 4
-XS_9 = 4
-XS_10 = 100
-
-XBH_1 = 5
-XBH_2 = 10
-XBH_3 = 20
-XBH_4 = 20
-XBH_5 = 200
-XBH_6 = 200
-XBH_7 = 200
-XBH_8 = 200
-XBH_9 = 200
-XBH_10 = 3000
-
-XBA_1 = 0.5
-XBA_2 = 1
-XBA_3 = 1
-XBA_4 = 1
-XBA_5 = 4
-XBA_6 = 4
-XBA_7 = 4
-XBA_8 = 4
-XBA_9 = 4
-XBA_10 = 100
-
-XP_1 = 0.5
-XP_2 = 1
-XP_3 = 1
-XP_4 = 1
-XP_5 = 4
-XP_6 = 4
-XP_7 = 4
-XP_8 = 4
-XP_9 = 4
-XP_10 = 100
-
-SO_1 = 1.3748
-SO_2 = 1.3748
-SO_3 = 1.3748
-SO_4 = 1.3748
-SO_5 = 1.3748
-SO_6 = 1.3748
-SO_7 = 1.3748
-SO_8 = 1.3748
-SO_9 = 1.3748
-SO_10 = 1.3748
-
-SNO_1 = 9.1948
-SNO_2 = 9.1948
-SNO_3 = 9.1948
-SNO_4 = 9.1948
-SNO_5 = 9.1948
-SNO_6 = 9.1948
-SNO_7 = 9.1948
-SNO_8 = 9.1948
-SNO_9 = 9.1948
-SNO_10 = 9.1948
-
-SNH_1 = 0.1585
-SNH_2 = 0.1585
-SNH_3 = 0.1585
-SNH_4 = 0.1585
-SNH_5 = 0.1585
-SNH_6 = 0.1585
-SNH_7 = 0.1585
-SNH_8 = 0.1585
-SNH_9 = 0.1585
-SNH_10 = 0.1585
-
-SND_1 = 0.5594
-SND_2 = 0.5594
-SND_3 = 0.5594
-SND_4 = 0.5594
-SND_5 = 0.5594
-SND_6 = 0.5594
-SND_7 = 0.5594
-SND_8 = 0.5594
-SND_9 = 0.5594
-SND_10 = 0.5594
-
-XND_1 = 0.5
-XND_2 = 1
-XND_3 = 1
-XND_4 = 1
-XND_5 = 4
-XND_6 = 4
-XND_7 = 4
-XND_8 = 4
-XND_9 = 4
-XND_10 = 100
-
-SALK_1 = 4.5646
-SALK_2 = 4.5646
-SALK_3 = 4.5646
-SALK_4 = 4.5646
-SALK_5 = 4.5646
-SALK_6 = 4.5646
-SALK_7 = 4.5646
-SALK_8 = 4.5646
-SALK_9 = 4.5646
-SALK_10 = 4.5646
-
-TSS_1 = 14.3255
-TSS_2 = 20.8756
-TSS_3 = 34.2948
-TSS_4 = 81.0276
-TSS_5 = 423.2035
-TSS_6 = 423.2035
-TSS_7 = 423.2035
-TSS_8 = 423.2035
-TSS_9 = 3.7106e+03
-TSS_10 = 7.3483e+03
-
-T_1 = 14.8581
-T_2 = 14.8581
-T_3 = 14.8581
-T_4 = 14.8581
-T_5 = 14.8581
-T_6 = 14.8581
-T_7 = 14.8581
-T_8 = 14.8581
-T_9 = 14.8581
-T_10 = 14.8581
-
-SD1_1 = 0
-SD1_2 = 0
-SD1_3 = 0
-SD1_4 = 0
-SD1_5 = 0
-SD1_6 = 0
-SD1_7 = 0
-SD1_8 = 0
-SD1_9 = 0
-SD1_10 = 0
-
-SD2_1 = 0
-SD2_2 = 0
-SD2_3 = 0
-SD2_4 = 0
-SD2_5 = 0
-SD2_6 = 0
-SD2_7 = 0
-SD2_8 = 0
-SD2_9 = 0
-SD2_10 = 0
-
-SD3_1 = 0
-SD3_2 = 0
-SD3_3 = 0
-SD3_4 = 0
-SD3_5 = 0
-SD3_6 = 0
-SD3_7 = 0
-SD3_8 = 0
-SD3_9 = 0
-SD3_10 = 0
-
-XD4_1 = 0
-XD4_2 = 0
-XD4_3 = 0
-XD4_4 = 0
-XD4_5 = 0
-XD4_6 = 0
-XD4_7 = 0
-XD4_8 = 0
-XD4_9 = 0
-XD4_10 = 0
-
-XD5_1 = 0
-XD5_2 = 0
-XD5_3 = 0
-XD5_4 = 0
-XD5_5 = 0
-XD5_6 = 0
-XD5_7 = 0
-XD5_8 = 0
-XD5_9 = 0
-XD5_10 = 0
-
-SETTLERINIT = np.array([SI_1, SI_2, SI_3, SI_4, SI_5, SI_6, SI_7, SI_8, SI_9, SI_10, SS_1, SS_2, SS_3, SS_4, SS_5, SS_6, SS_7, SS_8, SS_9, SS_10, XI_1, XI_2, XI_3, XI_4, XI_5, XI_6, XI_7, XI_8, XI_9, XI_10, XS_1, XS_2, XS_3, XS_4, XS_5, XS_6, XS_7, XS_8, XS_9, XS_10, XBH_1, XBH_2, XBH_3, XBH_4, XBH_5, XBH_6, XBH_7, XBH_8, XBH_9, XBH_10, XBA_1, XBA_2, XBA_3, XBA_4, XBA_5, XBA_6, XBA_7, XBA_8, XBA_9, XBA_10, XP_1, XP_2, XP_3, XP_4, XP_5, XP_6, XP_7, XP_8, XP_9, XP_10, SO_1, SO_2, SO_3, SO_4, SO_5, SO_6, SO_7, SO_8, SO_9, SO_10, SNO_1, SNO_2, SNO_3, SNO_4, SNO_5, SNO_6, SNO_7, SNO_8, SNO_9, SNO_10, SNH_1, SNH_2, SNH_3, SNH_4, SNH_5, SNH_6, SNH_7, SNH_8, SNH_9, SNH_10, SND_1, SND_2, SND_3, SND_4, SND_5, SND_6, SND_7, SND_8, SND_9, SND_10, XND_1, XND_2, XND_3, XND_4, XND_5, XND_6, XND_7, XND_8, XND_9, XND_10, SALK_1, SALK_2, SALK_3, SALK_4, SALK_5, SALK_6, SALK_7, SALK_8, SALK_9, SALK_10, TSS_1, TSS_2, TSS_3, TSS_4, TSS_5, TSS_6, TSS_7, TSS_8, TSS_9, TSS_10,
-                        T_1, T_2, T_3, T_4, T_5, T_6, T_7, T_8, T_9, T_10, SD1_1, SD1_2, SD1_3, SD1_4, SD1_5, SD1_6, SD1_7, SD1_8, SD1_9, SD1_10, SD2_1, SD2_2, SD2_3, SD2_4, SD2_5, SD2_6, SD2_7, SD2_8, SD2_9, SD2_10, SD3_1, SD3_2, SD3_3, SD3_4, SD3_5, SD3_6, SD3_7, SD3_8, SD3_9, SD3_10, XD4_1, XD4_2, XD4_3, XD4_4, XD4_5, XD4_6, XD4_7, XD4_8, XD4_9, XD4_10, XD5_1, XD5_2, XD5_3, XD5_4, XD5_5, XD5_6, XD5_7, XD5_8, XD5_9, XD5_10])
+settlerinit = np.ones(120)
+# settlerinit[0:90] = np.random.rand(90)
+settlerinit[90:120] = 0
 
 v0_max = 250
 v0 = 474
@@ -245,6 +26,7 @@ height = 4
 
 DIM = np.array([area, height])
 
+# layers are flexible, default model is a 10 layer sedimentation tank
 feedlayer = 5
 nooflayers = 10
 
