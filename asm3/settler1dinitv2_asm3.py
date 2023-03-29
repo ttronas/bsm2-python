9cefe0083b39304dd0d4a20512d5173157b101d8
ub97onag <maike.boehm@fau.de>
2023-03-29 09:35:36 +0200

    Initial commit

---
+++ asm3/settler1dinitv2_asm3.py
@@ -0,0 +1,246 @@
+import numpy as np
+
+# settlerinit = np.ones(190)
+# settlerinit[140:190] = 0
+
+SO_1 = 2
+SO_2 = 2
+SO_3 = 2
+SO_4 = 2
+SO_5 = 2
+SO_6 = 2
+SO_7 = 2
+SO_8 = 2
+SO_9 = 2
+SO_10 = 2
+
+SI_1 = 0.80801
+SI_2 = 0.80801
+SI_3 = 0.80801
+SI_4 = 0.80801
+SI_5 = 0.80801
+SI_6 = 0.80801
+SI_7 = 0.80801
+SI_8 = 0.80801
+SI_9 = 0.80801
+SI_10 = 0.80801
+
+SS_1 = 0.80801
+SS_2 = 0.80801
+SS_3 = 0.80801
+SS_4 = 0.80801
+SS_5 = 0.80801
+SS_6 = 0.80801
+SS_7 = 0.80801
+SS_8 = 0.80801
+SS_9 = 0.80801
+SS_10 = 0.80801
+
+SNH_1 = 0.67193
+SNH_2 = 0.67193
+SNH_3 = 0.67193
+SNH_4 = 0.67193
+SNH_5 = 0.67193
+SNH_6 = 0.67193
+SNH_7 = 0.67193
+SNH_8 = 0.67193
+SNH_9 = 0.67193
+SNH_10 = 0.67193
+
+SN2_1 = 13.5243
+SN2_2 = 13.5243
+SN2_3 = 13.5243
+SN2_4 = 13.5243
+SN2_5 = 13.5243
+SN2_6 = 13.5243
+SN2_7 = 13.5243
+SN2_8 = 13.5243
+SN2_9 = 13.5243
+SN2_10 = 13.5243
+
+SNO3_1 = 2
+SNO3_2 = 2
+SNO3_3 = 2
+SNO3_4 = 2
+SNO3_5 = 2
+SNO3_6 = 2
+SNO3_7 = 2
+SNO3_8 = 2
+SNO3_9 = 2
+SNO3_10 = 2
+
+SALK_1 = 3.8277
+SALK_2 = 3.8277
+SALK_3 = 3.8277
+SALK_4 = 3.8277
+SALK_5 = 3.8277
+SALK_6 = 3.8277
+SALK_7 = 3.8277
+SALK_8 = 3.8277
+SALK_9 = 3.8277
+SALK_10 = 3.8277
+
+XI_1 = 5
+XI_2 = 10
+XI_3 = 20
+XI_4 = 20
+XI_5 = 200
+XI_6 = 200
+XI_7 = 200
+XI_8 = 200
+XI_9 = 200
+XI_10 = 3000
+
+XS_1 = 0.5
+XS_2 = 1
+XS_3 = 1
+XS_4 = 1
+XS_5 = 4
+XS_6 = 4
+XS_7 = 4
+XS_8 = 4
+XS_9 = 4
+XS_10 = 100
+
+XBH_1 = 5
+XBH_2 = 10
+XBH_3 = 20
+XBH_4 = 20
+XBH_5 = 200
+XBH_6 = 200
+XBH_7 = 200
+XBH_8 = 200
+XBH_9 = 200
+XBH_10 = 3000
+
+XSTO_1 = 0.5
+XSTO_2 = 1
+XSTO_3 = 1
+XSTO_4 = 1
+XSTO_5 = 4
+XSTO_6 = 4
+XSTO_7 = 4
+XSTO_8 = 4
+XSTO_9 = 4
+XSTO_10 = 100
+
+XBA_1 = 0.5
+XBA_2 = 1
+XBA_3 = 1
+XBA_4 = 1
+XBA_5 = 4
+XBA_6 = 4
+XBA_7 = 4
+XBA_8 = 4
+XBA_9 = 4
+XBA_10 = 100
+
+TSS_1 = 10
+TSS_2 = 20
+TSS_3 = 30
+TSS_4 = 70
+TSS_5 = 400
+TSS_6 = 400
+TSS_7 = 400
+TSS_8 = 400
+TSS_9 = 400
+TSS_10 = 4000
+
+T_1 = 14.8581
+T_2 = 14.8581
+T_3 = 14.8581
+T_4 = 14.8581
+T_5 = 14.8581
+T_6 = 14.8581
+T_7 = 14.8581
+T_8 = 14.8581
+T_9 = 14.8581
+T_10 = 14.8581
+
+SD1_1 = 0
+SD1_2 = 0
+SD1_3 = 0
+SD1_4 = 0
+SD1_5 = 0
+SD1_6 = 0
+SD1_7 = 0
+SD1_8 = 0
+SD1_9 = 0
+SD1_10 = 0
+
+SD2_1 = 0
+SD2_2 = 0
+SD2_3 = 0
+SD2_4 = 0
+SD2_5 = 0
+SD2_6 = 0
+SD2_7 = 0
+SD2_8 = 0
+SD2_9 = 0
+SD2_10 = 0
+
+SD3_1 = 0
+SD3_2 = 0
+SD3_3 = 0
+SD3_4 = 0
+SD3_5 = 0
+SD3_6 = 0
+SD3_7 = 0
+SD3_8 = 0
+SD3_9 = 0
+SD3_10 = 0
+
+XD4_1 = 0
+XD4_2 = 0
+XD4_3 = 0
+XD4_4 = 0
+XD4_5 = 0
+XD4_6 = 0
+XD4_7 = 0
+XD4_8 = 0
+XD4_9 = 0
+XD4_10 = 0
+
+XD5_1 = 0
+XD5_2 = 0
+XD5_3 = 0
+XD5_4 = 0
+XD5_5 = 0
+XD5_6 = 0
+XD5_7 = 0
+XD5_8 = 0
+XD5_9 = 0
+XD5_10 = 0
+
+settlerinit = np.array([SO_1, SO_2, SO_3, SO_4, SO_5, SO_6, SO_7, SO_8, SO_9, SO_10, SI_1, SI_2, SI_3, SI_4, SI_5, SI_6, SI_7, SI_8, SI_9, SI_10,
+               SS_1, SS_2, SS_3, SS_4, SS_5, SS_6, SS_7, SS_8, SS_9, SS_10, SNH_1, SNH_2, SNH_3, SNH_4, SNH_5, SNH_6, SNH_7, SNH_8,
+               SNH_9, SNH_10, SN2_1, SN2_2, SN2_3, SN2_4, SN2_5, SN2_6, SN2_7, SN2_8, SN2_9, SN2_10, SNO3_1, SNO3_2, SNO3_3, SNO3_4,
+               SNO3_5, SNO3_6, SNO3_7, SNO3_8, SNO3_9, SNO3_10, SALK_1, SALK_2, SALK_3, SALK_4, SALK_5, SALK_6, SALK_7, SALK_8,
+               SALK_9, SALK_10, XI_1, XI_2, XI_3, XI_4, XI_5, XI_6, XI_7, XI_8, XI_9, XI_10, XS_1, XS_2, XS_3, XS_4, XS_5, XS_6, XS_7,
+               XS_8, XS_9, XS_10, XBH_1, XBH_2, XBH_3, XBH_4, XBH_5, XBH_6, XBH_7, XBH_8, XBH_9, XBH_10, XSTO_1, XSTO_2, XSTO_3,
+               XSTO_4, XSTO_5, XSTO_6, XSTO_7, XSTO_8, XSTO_9, XSTO_10, XBA_1, XBA_2, XBA_3, XBA_4, XBA_5, XBA_6, XBA_7, XBA_8,
+               XBA_9, XBA_10, TSS_1, TSS_2, TSS_3, TSS_4, TSS_5, TSS_6, TSS_7, TSS_8, TSS_9, TSS_10, T_1, T_2, T_3, T_4, T_5,
+               T_6, T_7, T_8, T_9, T_10, SD1_1, SD1_2, SD1_3, SD1_4, SD1_5, SD1_6, SD1_7, SD1_8, SD1_9, SD1_10, SD2_1, SD2_2,
+               SD2_3, SD2_4, SD2_5, SD2_6, SD2_7, SD2_8, SD2_9, SD2_10, SD3_1, SD3_2, SD3_3, SD3_4, SD3_5, SD3_6, SD3_7, SD3_8, SD3_9,
+               SD3_10, XD4_1, XD4_2, XD4_3, XD4_4, XD4_5, XD4_6, XD4_7, XD4_8, XD4_9, XD4_10, XD5_1, XD5_2, XD5_3, XD5_4,
+               XD5_5, XD5_6, XD5_7, XD5_8, XD5_9, XD5_10])
+
+v0_max = 250
+v0 = 474
+r_h = 0.000576
+r_p = 0.00286
+f_ns = 0.00228
+X_t = 3000
+
+settlerpar = np.array([v0_max, v0, r_h, r_p, f_ns, X_t])
+
+
+area = 1500
+height = 4
+
+dim = np.array([area, height])
+
+feedlayer = 5
+nooflayers = 10
+
+layer = np.array([feedlayer, nooflayers])
---
+++ asm3/settler1dv2_asm3.py
@@ -0,0 +1,315 @@
+import numpy as np
+from scipy.integrate import solve_ivp, odeint
+from numba import jit, njit
+
+
+# differential equations in function because @jit does not compile class methods in nopython mode
+@jit(nopython=True)
+def derivativess(t, ys, ys_in, sedpar, dim, layer, Qr, Qw, tempmodel):
+    indices_components = np.arange(20)
+    SO2, SI, SS, SNH4, SN2, SNOX, SALK, XI, XS, XH, XSTO, XA, XSS, Q, TEMP, SD1, SD2, SD3, XD4, XD5 = indices_components
+
+    vs = np.zeros(10)
+    Js = np.zeros(11)
+    Js_temp = np.zeros(10)
+    Jflow = np.zeros(11)
+
+    dys = np.zeros(190)
+
+    area = dim[0]
+    feedlayer = layer[0]
+    h = dim[1] / layer[1]
+    volume = area * dim[1]
+
+    eps = 0.01
+    v_in = ys_in[Q] / area
+    Q_f = ys_in[Q]
+    Q_u = Qr + Qw
+    Q_e = ys_in[Q] - Q_u
+    v_up = Q_e / area
+    v_dn = Q_u / area
+    # print(area)
+
+    ystemp = ys
+    ystemp[ystemp < 0.0] = 0.00001
+    ys[ys < 0.0] = 0.00001
+
+    # sedimentations velocity for each of the layers:
+    for i in range(10):
+        vs[i] = sedpar[1] * (np.exp(-sedpar[2] * (ystemp[i + XSS * 10] - sedpar[4] * ys_in[XSS])) - np.exp(
+            -sedpar[3] * (ystemp[i + XSS * 10] - sedpar[4] * ys_in[XSS])))
+        vs[vs > sedpar[0]] = sedpar[0]
+        vs[vs < 0.0] = 0.0
+
+
+    # sludge flux due to sedimentation for each layer (not taking into account X limit)
+    for i in range(10):
+        Js_temp[i] = vs[i] * ystemp[i + XSS * 10]
+
+    # sludge flux due to the liquid flow (upflow or downflow, depending on layer)
+    for i in range(11):
+        if i < (feedlayer - eps):
+            Jflow[i] = v_up * ystemp[i + XSS * 10]
+        else:
+            Jflow[i] = v_dn * ystemp[i - 1 + XSS * 10]
+
+    # sludge flux due to sedimentation of each layer:
+    for i in range(9):
+        if i < (feedlayer - 1 - eps) and ystemp[i + 1 + XSS * 10] <= sedpar[5]:
+            Js[i + 1] = Js_temp[i]
+        elif Js_temp[i] < Js_temp[i + 1]:
+            Js[i + 1] = Js_temp[i]
+        else:
+            Js[i + 1] = Js_temp[i + 1]
+
+    # soluble component S_O:
+    for i in range(10):
+        if i < (feedlayer - 1 - eps):
+            dys[i] = (-v_up * ystemp[i] + v_up * ystemp[i + 1]) / h
+        elif i > (feedlayer - eps):
+            dys[i] = (v_dn * ystemp[i - 1] - v_dn * ystemp[i]) / h
+        else:
+            dys[i] = (v_in * ys_in[SO2] - v_up * ystemp[i] - v_dn * ystemp[i]) / h
+
+
+    # soluble component S_I:
+    for i in range(10):
+        if i < (feedlayer - 1 - eps):
+            dys[i+10] = (-v_up * ystemp[i+10] + v_up * ystemp[i + 1+10]) / h
+        elif i > (feedlayer - eps):
+            dys[i+10] = (v_dn * ystemp[i - 1+10] - v_dn * ystemp[i+10]) / h
+        else:
+            dys[i+10] = (v_in * ys_in[SI] - v_up * ystemp[i+10] - v_dn * ystemp[i+10]) / h
+
+
+    # soluble component S_S:
+    for i in range(10):
+        if i < (feedlayer - 1 - eps):
+            dys[i + 20] = (-v_up * ystemp[i + 20] + v_up * ystemp[i + 1 + 20]) / h
+        elif i > (feedlayer - eps):
+            dys[i + 20] = (v_dn * ystemp[i - 1 + 20] - v_dn * ystemp[i + 20]) / h
+        else:
+            dys[i + 20] = (v_in * ys_in[SS] - v_up * ystemp[i + 20] - v_dn * ystemp[i + 20]) / h
+
+    # soluble component S_NH4:
+    for i in range(10):
+        if i < (feedlayer - 1 - eps):
+            dys[i + 30] = (-v_up * ystemp[i + 30] + v_up * ystemp[i + 1 + 30]) / h
+        elif i > (feedlayer - eps):
+            dys[i + 30] = (v_dn * ystemp[i - 1 + 30] - v_dn * ystemp[i + 30]) / h
+        else:
+            dys[i + 30] = (v_in * ys_in[SNH4] - v_up * ystemp[i + 30] - v_dn * ystemp[i + 30]) / h
+
+    # soluble component S_N2:
+    for i in range(10):
+        if i < (feedlayer - 1 - eps):
+            dys[i + 40] = (-v_up * ystemp[i + 40] + v_up * ystemp[i + 1 + 40]) / h
+        elif i > (feedlayer - eps):
+            dys[i + 40] = (v_dn * ystemp[i - 1 + 40] - v_dn * ystemp[i + 40]) / h
+        else:
+            dys[i + 40] = (v_in * ys_in[SN2] - v_up * ystemp[i + 40] - v_dn * ystemp[i + 40]) / h
+
+    # soluble component S_NOX:
+    for i in range(10):
+        if i < (feedlayer - 1 - eps):
+            dys[i + 50] = (-v_up * ystemp[i + 50] + v_up * ystemp[i + 1 + 50]) / h
+        elif i > (feedlayer - eps):
+            dys[i + 50] = (v_dn * ystemp[i - 1 + 50] - v_dn * ystemp[i + 50]) / h
+        else:
+            dys[i + 50] = (v_in * ys_in[SNOX] - v_up * ystemp[i + 50] - v_dn * ystemp[i + 50]) / h
+
+    # soluble component S_ALK:
+    for i in range(10):
+        if i < (feedlayer - 1 - eps):
+            dys[i + 60] = (-v_up * ystemp[i + 60] + v_up * ystemp[i + 1 + 60]) / h
+        elif i > (feedlayer - eps):
+            dys[i + 60] = (v_dn * ystemp[i - 1 + 60] - v_dn * ystemp[i + 60]) / h
+        else:
+            dys[i + 60] = (v_in * ys_in[SALK] - v_up * ystemp[i + 60] - v_dn * ystemp[i + 60]) / h
+
+    # particulate component X_I:
+    for i in range(10):
+        if i < (feedlayer - 1 - eps):
+            dys[i + 70] = ((ystemp[i + 70] / ystemp[i + 120]) * (-Jflow[i] - Js[i + 1]) + (
+                    ystemp[i - 1 + 70] / ystemp[i - 1 + 120]) * Js[i] + (ystemp[i + 1 + 70] / ystemp[i + 1 + 120]) *
+                           Jflow[i + 1]) / h
+        elif i > (feedlayer - eps):
+            dys[i + 70] = ((ystemp[i + 70] / ystemp[i + 120]) * (-Jflow[i + 1] - Js[i + 1]) + (
+                    ystemp[i - 1 + 70] / ystemp[i - 1 + 120]) * (Jflow[i] + Js[i])) / h
+        else:
+            dys[i + 70] = ((ystemp[i + 70] / ystemp[i + 120]) * (-Jflow[i] - Jflow[i + 1] - Js[i + 1]) + (
+                    ystemp[i - 1 + 70] / ystemp[i - 1 + 120]) * Js[i] + v_in * ys_in[XI]) / h
+
+    # particulate component X_S:
+    for i in range(10):
+        if i < (feedlayer - 1 - eps):
+            dys[i + 80] = ((ystemp[i + 80] / ystemp[i + 120]) * (-Jflow[i] - Js[i + 1]) + (
+                    ystemp[i - 1 + 80] / ystemp[i - 1 + 120]) * Js[i] + (ystemp[i + 1 + 80] / ystemp[i + 1 + 120]) *
+                           Jflow[i + 1]) / h
+        elif i > (feedlayer - eps):
+            dys[i + 80] = ((ystemp[i + 80] / ystemp[i + 120]) * (-Jflow[i + 1] - Js[i + 1]) + (
+                    ystemp[i - 1 + 80] / ystemp[i - 1 + 120]) * (Jflow[i] + Js[i])) / h
+        else:
+            dys[i + 80] = ((ystemp[i + 80] / ystemp[i + 120]) * (-Jflow[i] - Jflow[i + 1] - Js[i + 1]) + (
+                    ystemp[i - 1 + 80] / ystemp[i - 1 + 120]) * Js[i] + v_in * ys_in[XS]) / h
+
+    # particulate component X_H:
+    for i in range(10):
+        if i < (feedlayer - 1 - eps):
+            dys[i + 90] = ((ystemp[i + 90] / ystemp[i + 120]) * (-Jflow[i] - Js[i + 1]) + (
+                    ystemp[i - 1 + 90] / ystemp[i - 1 + 120]) * Js[i] + (ystemp[i + 1 + 90] / ystemp[i + 1 + 120]) *
+                           Jflow[i + 1]) / h
+        elif i > (feedlayer - eps):
+            dys[i + 90] = ((ystemp[i + 90] / ystemp[i + 120]) * (-Jflow[i + 1] - Js[i + 1]) + (
+                    ystemp[i - 1 + 90] / ystemp[i - 1 + 120]) * (Jflow[i] + Js[i])) / h
+        else:
+            dys[i + 90] = ((ystemp[i + 90] / ystemp[i + 120]) * (-Jflow[i] - Jflow[i + 1] - Js[i + 1]) + (
+                    ystemp[i - 1 + 90] / ystemp[i - 1 + 120]) * Js[i] + v_in * ys_in[XH]) / h
+
+    # particulate component X_STO:
+    for i in range(10):
+        if i < (feedlayer - 1 - eps):
+            dys[i + 100] = ((ystemp[i + 100] / ystemp[i + 120]) * (-Jflow[i] - Js[i + 1]) + (
+                    ystemp[i - 1 + 100] / ystemp[i - 1 + 120]) * Js[i] + (
+                                       ystemp[i + 1 + 100] / ystemp[i + 1 + 120]) *
+                           Jflow[i + 1]) / h
+        elif i > (feedlayer - eps):
+            dys[i + 100] = ((ystemp[i + 100] / ystemp[i + 120]) * (-Jflow[i + 1] - Js[i + 1]) + (
+                    ystemp[i - 1 + 100] / ystemp[i - 1 + 120]) * (Jflow[i] + Js[i])) / h
+        else:
+            dys[i + 100] = ((ystemp[i + 100] / ystemp[i + 120]) * (-Jflow[i] - Jflow[i + 1] - Js[i + 1]) + (
+                    ystemp[i - 1 + 100] / ystemp[i - 1 + 120]) * Js[i] + v_in * ys_in[XSTO]) / h
+
+    # particulate component X_A:
+    for i in range(10):
+        if i < (feedlayer - 1 - eps):
+            dys[i + 110] = ((ystemp[i + 110] / ystemp[i + 120]) * (-Jflow[i] - Js[i + 1]) + (
+                    ystemp[i - 1 + 110] / ystemp[i - 1 + 120]) * Js[i] + (ystemp[i + 1 + 110] / ystemp[i + 1 + 120]) *
+                           Jflow[i + 1]) / h
+        elif i > (feedlayer - eps):
+            dys[i + 110] = ((ystemp[i + 110] / ystemp[i + 120]) * (-Jflow[i + 1] - Js[i + 1]) + (
+                    ystemp[i - 1 + 110] / ystemp[i - 1 + 120]) * (Jflow[i] + Js[i])) / h
+        else:
+            dys[i + 110] = ((ystemp[i + 110] / ystemp[i + 120]) * (-Jflow[i] - Jflow[i + 1] - Js[i + 1]) + (
+                    ystemp[i - 1 + 110] / ystemp[i - 1 + 120]) * Js[i] + v_in * ys_in[XA]) / h
+
+    # particulate component X_SS:
+    for i in range(10):
+        if i < (feedlayer - 1 - eps):
+            dys[i + 120] = ((-Jflow[i] - Js[i + 1]) + Js[i] + Jflow[i + 1]) / h
+        elif i > (feedlayer - eps):
+            dys[i + 120] = ((-Jflow[i + 1] - Js[i + 1]) + (Jflow[i] + Js[i])) / h
+        else:
+            dys[i + 120] = ((-Jflow[i] - Jflow[i + 1] - Js[i + 1]) + Js[i] + v_in * ys_in[XSS]) / h
+
+    # Temperature:
+    for i in range(10):
+        if i < (feedlayer - 1 - eps):
+            dys[i + 130] = (-v_up * ystemp[i + 130] + v_up * ystemp[i + 1 + 130]) / h
+        elif i > (feedlayer - eps):
+            dys[i + 130] = (v_dn * ystemp[i - 1 + 130] - v_dn * ystemp[i + 130]) / h
+        else:
+            dys[i + 130] = (v_in * ys_in[TEMP] - v_up * ystemp[i + 130] - v_dn * ystemp[i + 130]) / h
+
+    # soluble component S_D1:
+    for i in range(10):
+        if i < (feedlayer - 1 - eps):
+            dys[i + 140] = (-v_up * ystemp[i + 140] + v_up * ystemp[i + 1 + 140]) / h
+        elif i > (feedlayer - eps):
+            dys[i + 140] = (v_dn * ystemp[i - 1 + 140] - v_dn * ystemp[i + 140]) / h
+        else:
+            dys[i + 140] = (v_in * ys_in[SD1] - v_up * ystemp[i + 140] - v_dn * ystemp[i + 140]) / h
+
+    # soluble component S_D2:
+    for i in range(10):
+        if i < (feedlayer - 1 - eps):
+            dys[i + 150] = (-v_up * ystemp[i + 150] + v_up * ystemp[i + 1 + 150]) / h
+        elif i > (feedlayer - eps):
+            dys[i + 150] = (v_dn * ystemp[i - 1 + 150] - v_dn * ystemp[i + 150]) / h
+        else:
+            dys[i + 150] = (v_in * ys_in[SD2] - v_up * ystemp[i + 150] - v_dn * ystemp[i + 150]) / h
+
+    # soluble component S_D3:
+    for i in range(10):
+        if i < (feedlayer - 1 - eps):
+            dys[i + 160] = (-v_up * ystemp[i + 160] + v_up * ystemp[i + 1 + 160]) / h
+        elif i > (feedlayer - eps):
+            dys[i + 160] = (v_dn * ystemp[i - 1 + 160] - v_dn * ystemp[i + 160]) / h
+        else:
+            dys[i + 160] = (v_in * ys_in[SD3] - v_up * ystemp[i + 160] - v_dn * ystemp[i + 160]) / h
+
+    # particulate component X_D4:
+    dys[170] = ((ys[170] / ys[120]) * (-Jflow[0] - Js[1]) + (ys[171] / ys[121]) * Jflow[1]) / h
+    for i in range(1, 10):
+        if i < (feedlayer - 1 - eps):
+            dys[i + 170] = ((ystemp[i + 170] / ystemp[i + 120]) * (-Jflow[i] - Js[i + 1]) + (
+                    ystemp[i - 1 + 170] / ystemp[i - 1 + 120]) * Js[i] + (
+                                        ystemp[i + 1 + 170] / ystemp[i + 1 + 120]) *
+                            Jflow[i + 1]) / h
+        elif i > (feedlayer - eps):
+            dys[i + 170] = ((ystemp[i + 170] / ystemp[i + 120]) * (-Jflow[i + 1] - Js[i + 1]) + (
+                    ystemp[i - 1 + 170] / ystemp[i - 1 + 120]) * (Jflow[i] + Js[i])) / h
+        else:
+            dys[i + 170] = ((ystemp[i + 170] / ystemp[i + 120]) * (-Jflow[i] - Jflow[i + 1] - Js[i + 1]) + (
+                    ystemp[i - 1 + 170] / ystemp[i - 1 + 120]) * Js[i] + v_in * ys_in[XD4]) / h
+
+    # particulate component X_D5:
+    dys[180] = ((ys[180] / ys[120]) * (-Jflow[0] - Js[1]) + (ys[181] / ys[121]) * Jflow[1]) / h
+    for i in range(1, 10):
+        if i < (feedlayer - 1 - eps):
+            dys[i + 180] = ((ystemp[i + 180] / ystemp[i + 120]) * (-Jflow[i] - Js[i + 1]) + (
+                    ystemp[i - 1 + 180] / ystemp[i - 1 + 120]) * Js[i] + (ystemp[i + 1 + 180] / ystemp[i + 1 + 120]) *
+                            Jflow[i + 1]) / h
+        elif i > (feedlayer - eps):
+            dys[i + 180] = ((ystemp[i + 180] / ystemp[i + 120]) * (-Jflow[i + 1] - Js[i + 1]) + (
+                    ystemp[i - 1 + 180] / ystemp[i - 1 + 120]) * (Jflow[i] + Js[i])) / h
+        else:
+            dys[i + 180] = ((ystemp[i + 180] / ystemp[i + 120]) * (-Jflow[i] - Jflow[i + 1] - Js[i + 1]) + (
+                    ystemp[i - 1 + 180] / ystemp[i - 1 + 120]) * Js[i] + v_in * ys_in[XD5]) / h
+
+    return dys
+
+
+class Settler:
+    def __init__(self, dim, layer, Qr, Qw, ys0, sedpar, tempmodel):
+
+        self.dim = dim
+        self.layer = layer
+        self.Qr = Qr
+        self.Qw = Qw
+        self.ys0 = ys0
+        self.sedpar = sedpar
+        self.tempmodel = tempmodel
+
+    def outputs(self, timestep, step, ys_in):
+
+        ys_out_all = np.zeros(193)
+        ys_out = np.zeros(20)
+        ys_eff = np.zeros(20)
+        t_eval = np.array([step, step + timestep])
+        # def derivativess(t, ys, ys_in, sedpar, dim, layer, Qr, Qw, tempmodel):
+        odes = odeint(derivativess, self.ys0, t_eval, tfirst=True, args=(ys_in, self.sedpar, self.dim, self.layer, self.Qr, self.Qw, self.tempmodel))
+        ys_int = odes[1]
+
+        self.ys0 = ys_int
+
+        for i in range(10):
+            for j in range(19):
+                ys_out_all[(i * 19) + j] = ys_int[i + j * 10]
+            if not self.tempmodel:
+                ys_out_all[(i * 19) + 13] = ys_in[14]
+
+        # flow rates out of the clarifier:
+        ys_out_all[190] = ys_in[13] - self.Qr - self.Qw
+        ys_out_all[191] = self.Qr
+        ys_out_all[192] = self.Qw
+
+        ys_out[0:13] = ys_out_all[171:184]
+        ys_out[13] = self.Qr
+        ys_out[14:20] = ys_out_all[184:190]
+
+        ys_eff[0:13] = ys_out_all[0:13]
+        ys_eff[13] = ys_out_all[190]
+        ys_eff[14:20] = ys_out_all[13:19]
+
+        return ys_out, ys_eff
