deadeadd1f4c451ea0aca71a225969fb26bbb8dc
ub97onag <maike.boehm@fau.de>
2023-05-27 08:19:16 +0200

    Second commit

--- asm1/average_asm1.py
+++ asm1/average_asm1.py
@@ -4,13 +4,28 @@ indices_components = np.arange(21)
 SI, SS, XI, XS, XBH, XBA, XP, SO, SNO, SNH, SND, XND, SALK, TSS, Q, TEMP, SD1, SD2, SD3, XD4, XD5 = indices_components
 
 
-# simetime: array der alle Zeitschritte enthält
-# y_array enthält alle Werte von z.B. Effluent
-# evaltime ist start und endzeit von Intervall, das für Berechnung ist (z.B. 7 bis 14 d)
 def averages(y_array, sampleinterval, evaltime):
-    y_average = np.zeros(25)
-    for n in range(25):
+    """Returns an array containing the average values of the components during a certain evaluation time
+    in a certain part of the plant
+
+    Parameters
+    ----------
+    y_array : np.ndarray
+        Values of the components at every time step of the evaluation time
+    sampleinterval : int or float
+        Time step of the evaluation time in days
+    evaltime : np.ndarray
+        Starting and end point of the evaluation time in days
+
+    Returns
+    -------
+    np.ndarray
+        Array containing the average values of the components during the evaluation time
+    """
+
+    size_array = int(len(y_array[0]))
+    y_average = np.zeros(size_array)
+    for n in range(size_array):
         y_average[n] = np.sum(sampleinterval * y_array[:, Q] * y_array[:, n]) / np.sum(sampleinterval * y_array[:, Q])
     y_average[Q] = np.sum(sampleinterval * y_array[:, Q]) / (evaltime[1] - evaltime[0])
-    # print(y_average[Q])
     return y_average
