a2a5a2e34034d973ea47e38235721d92aacbd936
Jonas Miederer <jonas.miederer@fau.de>
2023-10-03 17:08:12 +0200

    jitted more ASM1 and ASM3 as well as Settler functions

--- asm1/settler1d_asm1.py
+++ asm1/settler1d_asm1.py
@@ -172,6 +172,109 @@ def settlerequations(t, ys, ys_in, sedpar, dim, layer, Qr, Qw, tempmodel):
     return dys
 
 
+@jit(nopython=True)
+def get_outputs(ys_int, ys_in, nooflayers, tempmodel, Qr, Qw, asm1par):
+    """
+    Returns the values of the 21 components (13 ASM1 components, TSS, Q, T and 5 dummy states)
+    in the effluent (top layer of settler) and underflow (bottom layer of settler) at the current time step after the integration
+
+    Parameters
+    ----------
+    ys_int : np.ndarray
+        Solution of the differential equations from the Solver
+    ys_in : np.ndarray
+        Settler inlet concentrations of the 21 components (13 ASM1 components, TSS, Q, T and 5 dummy states)
+    nooflayers : int
+        Number of layers in the settler
+    tempmodel : bool
+        If true, differential equation for the wastewater temperature is used,
+        otherwise influent wastewater temperature is just passed through the settler
+    Qr : int
+        Return sludge flow rate
+    Qw : int
+        flow rate of waste sludge
+    asm1par : np.ndarray
+        24 parameters needed for ASM1 equations
+    
+    Returns
+    -------
+    (np.ndarray, np.ndarray)
+        Tuple containing two arrays:
+            ys_out: Array containing the values of the 21 components (13 ASM1 components, TSS, Q, T and 5 dummy states)
+            in the effluent (top layer of settler) at the current time step after the integration
+            ys_eff: Array containing the values of the 21 components (13 ASM1 components, TSS, Q, T and 5 dummy states)
+            in the underflow (bottom layer of settler) at the current time step after the integration
+    """
+    ys_out = np.zeros(21)
+    ys_eff = np.zeros(25)
+    # underflow
+    ys_out[SI] = ys_int[nooflayers-1]
+    ys_out[SS] = ys_int[2*nooflayers-1]
+    ys_out[SO] = ys_int[3*nooflayers-1]
+    ys_out[SNO] = ys_int[4*nooflayers-1]
+    ys_out[SNH] = ys_int[5*nooflayers-1]
+    ys_out[SND] = ys_int[6*nooflayers-1]
+    ys_out[SALK] = ys_int[7*nooflayers-1]
+    ys_out[TSS] = ys_int[8*nooflayers-1]
+    if tempmodel:
+        ys_out[TEMP] = ys_int[9*nooflayers-1]
+    else:
+        ys_out[TEMP] = ys_in[TEMP]
+    ys_out[SD1] = ys_int[10*nooflayers-1]
+    ys_out[SD2] = ys_int[11*nooflayers-1]
+    ys_out[SD3] = ys_int[12*nooflayers-1]
+
+    ys_out[XI] = ys_out[TSS] / ys_in[TSS] * ys_in[XI]
+    ys_out[XS] = ys_out[TSS] / ys_in[TSS] * ys_in[XS]
+    ys_out[XBH] = ys_out[TSS] / ys_in[TSS] * ys_in[XBH]
+    ys_out[XBA] = ys_out[TSS] / ys_in[TSS] * ys_in[XBA]
+    ys_out[XP] = ys_out[TSS] / ys_in[TSS] * ys_in[XP]
+    ys_out[XND] = ys_out[TSS] / ys_in[TSS] * ys_in[XND]
+    ys_out[XD4] = ys_out[TSS] / ys_in[TSS] * ys_in[XD4]
+    ys_out[XD5] = ys_out[TSS] / ys_in[TSS] * ys_in[XD5]
+
+    ys_out[Q] = Qr
+
+    # effluent
+    ys_eff[SI] = ys_int[0]
+    ys_eff[SS] = ys_int[nooflayers]
+    ys_eff[SO] = ys_int[2*nooflayers]
+    ys_eff[SNO] = ys_int[3*nooflayers]
+    ys_eff[SNH] = ys_int[4*nooflayers]
+    ys_eff[SND] = ys_int[5*nooflayers]
+    ys_eff[SALK] = ys_int[6*nooflayers]
+    ys_eff[TSS] = ys_int[7*nooflayers]
+    if tempmodel:
+        ys_eff[TEMP] = ys_int[8*nooflayers]
+    else:
+        ys_eff[TEMP] = ys_in[TEMP]
+    ys_eff[SD1] = ys_int[9*nooflayers]
+    ys_eff[SD2] = ys_int[10*nooflayers]
+    ys_eff[SD3] = ys_int[11*nooflayers]
+
+    ys_eff[XI] = ys_eff[TSS] / ys_in[TSS] * ys_in[XI]
+    ys_eff[XS] = ys_eff[TSS] / ys_in[TSS] * ys_in[XS]
+    ys_eff[XBH] = ys_eff[TSS] / ys_in[TSS] * ys_in[XBH]
+    ys_eff[XBA] = ys_eff[TSS] / ys_in[TSS] * ys_in[XBA]
+    ys_eff[XP] = ys_eff[TSS] / ys_in[TSS] * ys_in[XP]
+    ys_eff[XND] = ys_eff[TSS] / ys_in[TSS] * ys_in[XND]
+    ys_eff[XD4] = ys_eff[TSS] / ys_in[TSS] * ys_in[XD4]
+    ys_eff[XD5] = ys_eff[TSS] / ys_in[TSS] * ys_in[XD5]
+
+    ys_eff[Q] = ys_in[Q] - Qw - Qr
+
+    # additional values to compare:
+    # Kjeldahl N concentration:
+    ys_eff[21] = ys_eff[SNH] + ys_eff[SND] + ys_eff[XND] + asm1par[17] * (ys_eff[XBH] + ys_eff[XBA]) + asm1par[18] * (ys_eff[XP] + ys_eff[XI])
+    # total N concentration:
+    ys_eff[22] = ys_eff[21] + ys_eff[SNO]
+    # total COD concentration:
+    ys_eff[23] = ys_eff[SS] + ys_eff[SI] + ys_eff[XS] + ys_eff[XI] + ys_eff[XBH] + ys_eff[XBA] + ys_eff[XP]
+    # BOD5 concentration:
+    ys_eff[24] = 0.25 * (ys_eff[SS] + ys_eff[XS] + (1-asm1par[16]) * (ys_eff[XBH] + ys_eff[XBA]))
+
+    return ys_out, ys_eff
+
 class Settler:
     def __init__(self, dim, layer, Qr, Qw, ys0, sedpar, asm1par, tempmodel):
         """
@@ -228,8 +331,6 @@ class Settler:
         """
 
         nooflayers = self.layer[1]
-        ys_out = np.zeros(21)
-        ys_eff = np.zeros(25)
         ys_TSS = np.zeros(nooflayers)
         t_eval = np.array([step, step + timestep])
 
@@ -237,71 +338,6 @@ class Settler:
         ys_int = odes[1]
 
         self.ys0 = ys_int
-
-        # underflow
-        ys_out[SI] = ys_int[nooflayers-1]
-        ys_out[SS] = ys_int[2*nooflayers-1]
-        ys_out[SO] = ys_int[3*nooflayers-1]
-        ys_out[SNO] = ys_int[4*nooflayers-1]
-        ys_out[SNH] = ys_int[5*nooflayers-1]
-        ys_out[SND] = ys_int[6*nooflayers-1]
-        ys_out[SALK] = ys_int[7*nooflayers-1]
-        ys_out[TSS] = ys_int[8*nooflayers-1]
-        if self.tempmodel:
-            ys_out[TEMP] = ys_int[9*nooflayers-1]
-        else:
-            ys_out[TEMP] = ys_in[TEMP]
-        ys_out[SD1] = ys_int[10*nooflayers-1]
-        ys_out[SD2] = ys_int[11*nooflayers-1]
-        ys_out[SD3] = ys_int[12*nooflayers-1]
-
-        ys_out[XI] = ys_out[TSS] / ys_in[TSS] * ys_in[XI]
-        ys_out[XS] = ys_out[TSS] / ys_in[TSS] * ys_in[XS]
-        ys_out[XBH] = ys_out[TSS] / ys_in[TSS] * ys_in[XBH]
-        ys_out[XBA] = ys_out[TSS] / ys_in[TSS] * ys_in[XBA]
-        ys_out[XP] = ys_out[TSS] / ys_in[TSS] * ys_in[XP]
-        ys_out[XND] = ys_out[TSS] / ys_in[TSS] * ys_in[XND]
-        ys_out[XD4] = ys_out[TSS] / ys_in[TSS] * ys_in[XD4]
-        ys_out[XD5] = ys_out[TSS] / ys_in[TSS] * ys_in[XD5]
-
-        ys_out[Q] = self.Qr
-
-        # effluent
-        ys_eff[SI] = ys_int[0]
-        ys_eff[SS] = ys_int[nooflayers]
-        ys_eff[SO] = ys_int[2*nooflayers]
-        ys_eff[SNO] = ys_int[3*nooflayers]
-        ys_eff[SNH] = ys_int[4*nooflayers]
-        ys_eff[SND] = ys_int[5*nooflayers]
-        ys_eff[SALK] = ys_int[6*nooflayers]
-        ys_eff[TSS] = ys_int[7*nooflayers]
-        if self.tempmodel:
-            ys_eff[TEMP] = ys_int[8*nooflayers]
-        else:
-            ys_eff[TEMP] = ys_in[TEMP]
-        ys_eff[SD1] = ys_int[9*nooflayers]
-        ys_eff[SD2] = ys_int[10*nooflayers]
-        ys_eff[SD3] = ys_int[11*nooflayers]
-
-        ys_eff[XI] = ys_eff[TSS] / ys_in[TSS] * ys_in[XI]
-        ys_eff[XS] = ys_eff[TSS] / ys_in[TSS] * ys_in[XS]
-        ys_eff[XBH] = ys_eff[TSS] / ys_in[TSS] * ys_in[XBH]
-        ys_eff[XBA] = ys_eff[TSS] / ys_in[TSS] * ys_in[XBA]
-        ys_eff[XP] = ys_eff[TSS] / ys_in[TSS] * ys_in[XP]
-        ys_eff[XND] = ys_eff[TSS] / ys_in[TSS] * ys_in[XND]
-        ys_eff[XD4] = ys_eff[TSS] / ys_in[TSS] * ys_in[XD4]
-        ys_eff[XD5] = ys_eff[TSS] / ys_in[TSS] * ys_in[XD5]
-
-        ys_eff[Q] = ys_in[Q] - self.Qw - self.Qr
-
-        # additional values to compare:
-        # Kjeldahl N concentration:
-        ys_eff[21] = ys_eff[SNH] + ys_eff[SND] + ys_eff[XND] + self.asm1par[17] * (ys_eff[XBH] + ys_eff[XBA]) + self.asm1par[18] * (ys_eff[XP] + ys_eff[XI])
-        # total N concentration:
-        ys_eff[22] = ys_eff[21] + ys_eff[SNO]
-        # total COD concentration:
-        ys_eff[23] = ys_eff[SS] + ys_eff[SI] + ys_eff[XS] + ys_eff[XI] + ys_eff[XBH] + ys_eff[XBA] + ys_eff[XP]
-        # BOD5 concentration:
-        ys_eff[24] = 0.25 * (ys_eff[SS] + ys_eff[XS] + (1-self.asm1par[16]) * (ys_eff[XBH] + ys_eff[XBA]))
+        ys_out, ys_eff = get_outputs(ys_int, ys_in, nooflayers, self.tempmodel, self.Qr, self.Qw, self.asm1par)
 
         return ys_out, ys_eff
