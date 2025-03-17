3a6eb09b5934ba4d0d61f3e6a1e6e76b2f99620d
Nick Salomon <nick.salomon@fau.de>
2025-03-17 17:10:33 +0100

    Test changelog fix2 #5

--- docs/scripts/conf.py
+++ docs/scripts/conf.py
@@ -6,8 +6,8 @@ if not os.path.exists('./git-lfs'):
     os.system('wget https://github.com/git-lfs/git-lfs/releases/download/v3.6.1/git-lfs-linux-amd64-v3.6.1.tar.gz')
     os.system('tar xvfz git-lfs-linux-amd64-v3.6.1.tar.gz')
     os.system('./git-lfs install')  # make lfs available in current repository
-    os.system('./git-lfs prune --force')
-    os.system('./git-lfs untrack CHANGELOG.md')
+    os.system('./git-lfs prune --force')  # delete old git lfs files from local storage
+    os.system('./git-lfs untrack CHANGELOG.md')  # remove git lfs paths from git attributes
+    os.system('./git-lfs track "*.svg"')  # track .svg files
     os.system('./git-lfs fetch')  # download content from remote
     os.system('./git-lfs checkout')  # make local files to have the real content on them
-    os.system('./git-lfs ls-files')
