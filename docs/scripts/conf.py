7aa6707b0b7a8afeac0638ec870b15a8438b5672
Nick Salomon <nick.salomon@fau.de>
2025-03-17 15:01:28 +0100

    Test changelog fix

--- docs/scripts/conf.py
+++ docs/scripts/conf.py
@@ -6,5 +6,6 @@ if not os.path.exists('./git-lfs'):
     os.system('wget https://github.com/git-lfs/git-lfs/releases/download/v2.7.1/git-lfs-linux-amd64-v2.7.1.tar.gz')
     os.system('tar xvfz git-lfs-linux-amd64-v2.7.1.tar.gz')
     os.system('./git-lfs install')  # make lfs available in current repository
-    os.system('./git-lfs fetch')  # download content from remote
+    os.system('./git-lfs pull --exclude="CHANGELOG.md"')  # download content from remote
     os.system('./git-lfs checkout')  # make local files to have the real content on them
+    os.system('./git-lfs pull --exclude="CHANGELOG.md"')  # 
