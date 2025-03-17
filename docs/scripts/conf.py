e0fdbb7220b1294b7be522f9d3071f5a36b23de2
Nick Salomon <nick.salomon@fau.de>
2025-03-17 17:58:48 +0100

    Test changelog fix4 #5

--- docs/scripts/conf.py
+++ docs/scripts/conf.py
@@ -8,7 +8,3 @@ if not os.path.exists('./git-lfs'):
     os.system('./git-lfs install')  # make lfs available in current repository
     os.system('./git-lfs fetch')  # download content from remote
     os.system('./git-lfs checkout')  # make local files to have the real content on the
-    os.system('./git-lfs untrack CHANGELOG.md')  # remove git lfs paths from git attributes
-    os.system('./git-lfs track "*.svg"')  # track .svg files
-    os.system('./git add .gitattributes')
-    # os.system('./git-lfs prune --force')  # delete old git lfs files from local storage
