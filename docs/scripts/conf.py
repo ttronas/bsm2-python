a30b0eae5b982b4dade3425d2d255388e7a8282a
Nick Salomon <nick.salomon@fau.de>
2025-03-18 10:25:26 +0100

    Test changelog fix3 #6

--- docs/scripts/conf.py
+++ docs/scripts/conf.py
@@ -4,9 +4,8 @@
 import os
 if not os.path.exists('./git-lfs'):
     os.system('wget https://github.com/git-lfs/git-lfs/releases/download/v2.7.1/git-lfs-linux-amd64-v2.7.1.tar.gz')
-    os.system('tar xvfz git-lfs-linux-amd64-v2.7.1.tar.gz')
-    os.system('./git-lfs install')  # make lfs available in current repository
-    os.system('./gif-lfs prune')
-    os.system('./git config lfs.url https://gitlab.rrze.fau.de/evt/klaeffizient/bsm2-python')
-    os.system('./git-lfs fetch')  # download content from remote
-    os.system('./git-lfs checkout')  # make local files to have the real content on the
+    os.system('mkdir -p git-lfs-extracted')  # create new directory
+    os.system('tar xvfz git-lfs-linux-amd64-v2.7.1.tar.gz -C git-lfs-extracted')  # extract into new directory
+    os.system('./git-lfs-extracted/git-lfs install')  # make lfs available in current repository
+    os.system('./git-lfs-extracted/git-lfs fetch')  # download content from remote
+    os.system('./git-lfs-extracted/git-lfs checkout')  # make local files to have the real content on them
